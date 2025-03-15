d767e37f159ae41b65ec190362c164da33cddef8
MarcusDelPino <marcusdelpino14@gmail.com>
2025-03-15 17:26:38 -0300

    feat: Esse comite coloca os Repository e os Services, e o Jpa

---
+++ src/main/java/dev/java10x/CadastrosDeNinjas/MIssoes/MissoesRipository.java
@@ -0,0 +1,7 @@
+package dev.java10x.CadastrosDeNinjas.MIssoes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissoesRipository extends JpaRepository<MissoesModel, Long> {
+
+}
