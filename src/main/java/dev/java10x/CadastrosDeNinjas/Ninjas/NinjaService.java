14bc1e3ae660ef0287c9770fd611da22c230ad2b
MarcusDelPino <marcusdelpino14@gmail.com>
2025-03-21 22:58:55 -0300

    feat: inclui a a versao do DTO alterando o foco no DTO

--- src/main/java/dev/java10x/CadastrosDeNinjas/Ninjas/NinjaService.java
+++ src/main/java/dev/java10x/CadastrosDeNinjas/Ninjas/NinjaService.java
@@ -11,9 +11,11 @@ import java.util.Optional;
 public class NinjaService {
 
     private NinjaRepository ninjaRepository;
+    private NinjaMapper ninjaMapper;
 
-    public NinjaService(NinjaRepository ninjaRepository) {
+    public NinjaService(NinjaRepository ninjaRepository, NinjaMapper ninjaMapper) {
         this.ninjaRepository = ninjaRepository;
+        this.ninjaMapper = ninjaMapper;
     }
 
     //listar todos meus ninjas
@@ -26,8 +28,10 @@ public class NinjaService {
         return ninjaPorId.orElse(null);
     }
     //criar ninjas
-    public NinjaModel criarNinjas(NinjaModel ninja){
-        return ninjaRepository.save(ninja);
+    public NinjaDTO criarNinjas(NinjaDTO ninjaDTO){
+        NinjaModel ninja = ninjaMapper.map(ninjaDTO);
+        ninja = ninjaRepository.save(ninja);
+        return ninjaMapper.map(ninja);
     }
 
     //deletar ninja - tem que ser um metodo void
