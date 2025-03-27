7d3c2036d411c0b338ef8d59e1c534f9f1f339b0
MarcusDelPino <marcusdelpino14@gmail.com>
2025-03-26 22:27:41 -0300

    finalizando

--- src/main/java/dev/java10x/CadastrosDeNinjas/MIssoes/MissoesController.java
+++ src/main/java/dev/java10x/CadastrosDeNinjas/MIssoes/MissoesController.java
@@ -1,6 +1,11 @@
 package dev.java10x.CadastrosDeNinjas.MIssoes;
 
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,31 +21,75 @@ public class MissoesController {
     }
 
     @PostMapping("/criarmissoes")
-    public MissoesDTO criarMissoes(@RequestBody MissoesDTO missoesDTO){
-        return missoesService.criarMissoes(missoesDTO);
+    @Operation(summary = "Cria uma nova missão", description = "criar uma nova missão, e insere no banco de dados.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Missão criada com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Erro na criação da missão")
+    })
+    public ResponseEntity<String> criarMissoes(@RequestBody MissoesDTO missoesDTO){
+        MissoesDTO missaoDTO = missoesService.criarMissoes(missoesDTO);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body("Missão criada com sucesso! ID: " + missaoDTO.getId() + " nome: " + missaoDTO.getNome());
     }
     // visualizar todas as missoes
+    @Operation(summary = "Lista todos os ninjas", description = "Lista todos os ninjas do banco de dados.")
     @GetMapping("/listarmissoes")
-    public List<MissoesDTO> missoes(){
-        return missoesService.listarMissoes();
+    public ResponseEntity<List> missoes(){
+        return ResponseEntity.ok(missoesService.listarMissoes());
     }
 
     //lista missao por id
+    @Operation(summary = "Lista a missão pelo seu id", description = "Busca ua missão pelo seu id!")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Missão encontrada com sucesso"),
+            @ApiResponse(responseCode = "400", description = "ID da missão não existe!")
+    })
     @GetMapping("/mostrarmissao/{id}")
-    public MissoesDTO mostrarMissaoPorId(@PathVariable Long id){
-        return missoesService.mostrarMissaoPorId(id);
+    public ResponseEntity<?> mostrarMissaoPorId(@PathVariable Long id){
+        MissoesDTO missoes =  missoesService.mostrarMissaoPorId(id);
+        if (missoesService.mostrarMissaoPorId(id) != null){
+            return ResponseEntity.ok(missoes);
+        }else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Essa missão ID não existe!");
+        }
     }
 
     //deletar misssao
+    @Operation(summary = "Deleta a missão pelo id", description = "deleta a missão do banco de dados")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "missão apagada com sucesso"),
+            @ApiResponse(responseCode = "400", description = "ID da missão não existe!")
+    })
     @DeleteMapping("/deletar/{id}")
-    public void deletarMissao(@PathVariable Long id){
-        missoesService.deletarMissao(id);
+    public ResponseEntity<String> deletarMissao(@PathVariable Long id){
+        if (missoesService.mostrarMissaoPorId(id) != null){
+            missoesService.deletarMissao(id);
+            return ResponseEntity.ok("Missão de id: " + id + " deletada com sucesso!");
+        }else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Id da missão não encontrado!");
+        }
     }
 
     // atualizar por id
+    @Operation(summary = "Atualiza missão usando o id", description = "faz a atualização do missão usando ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Missão atualizada com sucesso"),
+            @ApiResponse(responseCode = "400", description = "ID do missão não existe!")
+    })
     @PutMapping("/atualizar/{id}")
-    public MissoesDTO atualizarMissao(@RequestBody MissoesDTO missoesAtualizada, @PathVariable Long id ){
-        return missoesService.atualizarMissao(id, missoesAtualizada);
+    public ResponseEntity<String> atualizarMissao(@RequestBody MissoesDTO missoesAtualizada, @PathVariable Long id ){
+
+        if (missoesService.mostrarMissaoPorId(id) != null){
+            missoesService.atualizarMissao(id, missoesAtualizada);
+            return ResponseEntity.status(HttpStatus.CREATED)
+                    .body("A atualização da missão de id: " + id + " foi atualizada com sucesso!");
+        }else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Id da missão não existe!");
+        }
+
     }
 
 
